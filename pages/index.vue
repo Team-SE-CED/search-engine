b1a068fe2a51dbfd274ebce2de9b5ce73588f81e
Eve <navinielvin@gmail.com>
2024-09-19 23:13:42 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>
+            <NuxtLink to="/login">Login</NuxtLink>
+        </h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
