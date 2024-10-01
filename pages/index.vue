37ed623ea3eace566ee9f40ec820cbf0e9913512
Eve <navinielvin@gmail.com>
2024-10-01 22:51:16 +0800

    refactor update

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,9 @@
 <template>
-<<<<<<< HEAD
-  <div class="container">
-    <SearchMainTitle />
-  </div>
   <div>
-    <SearchArea />
+    <h1>
+      <NuxtLink to="/welcome">Login</NuxtLink>
+    </h1>
   </div>
-=======
-    <div>
-        <h1>
-            <NuxtLink to="/welcome">Login</NuxtLink>
-        </h1>
-    </div>
->>>>>>> cc23c790cce469d9557b09f0b9bcbbe9980e168d
 </template>
 
 <script setup lang="ts"></script>
