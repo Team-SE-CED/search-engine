10349b2840fc8bb49b2d95afb6bb96c2f250ef64
Your Name <johnvjimar@gmail.com>
2024-10-02 19:00:58 +0800

    refactor(update): restructure components

--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,11 @@
 <template>
-<<<<<<<< HEAD:pages/login.vue
   <div>
     <LoginPage />
 
   </div>
-========
-    <div>
-        <SelectorPage/>  
-    </div>
->>>>>>>> 9e15a2388ed295cdc30ff7a872cd19cce9ab8d92:pages/selector.vue
+  <div>
+    <SelectorPage />
+  </div>
 </template>
 
 <script setup lang="ts">
