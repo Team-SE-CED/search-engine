957aaa7d6bfdafb1f4b2f281887805238abcf383
sggenDebugs <inigopgonzalez@su.edu.ph>
2024-10-05 23:05:01 +0800

    feat: create api to get email from supabase

---
+++ server/api/get-email/index.get.ts
@@ -0,0 +1,23 @@
+import { serverSupabaseClient } from '#supabase/server';
+
+export default defineEventHandler(async (event) => {
+  // read email from request Body
+  const body = await readBody(event);
+  const { email } = body;
+
+  const client = await serverSupabaseClient(event);
+
+  // check if existing user already exists
+  const { data, error } = await client
+    .from('auth.users')
+    .select('email')
+    .eq('email', email )
+    .single();
+  
+  if (error) {
+    // if no record found, email does not exist
+    return { exists: false };
+  }
+  // if email exists, return true
+  return { exists: !!data };
+})
