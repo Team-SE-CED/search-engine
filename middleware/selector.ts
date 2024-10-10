0e9a79db6424368de2497e95d05af7d0cfa8f59f
sggenDebugs <inigopgonzalez@su.edu.ph>
2024-10-10 15:42:36 +0800

    refactor remove

--- middleware/selector.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    if (to.path !== '/selector') {
-        // setting the redirect code to '301 Moved Permanently'
-        return navigateTo('/selector', { redirectCode: 301 })
-      }
-    else {
-      return navigateTo('/login', { redirectCode: 301 })
-    }
-    })
