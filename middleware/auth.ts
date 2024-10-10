caa555f44a80c9426a1d187b58d5b7597f541df8
sggenDebugs <inigopgonzalez@su.edu.ph>
2024-10-10 08:13:22 +0800

    feat: add auth middleware again

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,11 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.path !== "/login") {
-    // setting the redirect code to '301 Moved Permanently'
-    return navigateTo("/login", { redirectCode: 301 });
+// middleware/auth.js
+import { supabase } from '~/supabase'; // Make sure your supabase client is set up
+
+export default async function ({ redirect }) {
+  const user = supabase.auth.user(); // Check if a user is logged in
+
+  if (!user) {
+    // If no user is found, redirect to login page
+    return redirect('/login');
   }
-});
+}
