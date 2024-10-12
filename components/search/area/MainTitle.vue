465785327aac17cf472844a641a5b5e787bc07e6
Your Name <johnvjimar@gmail.com>
2024-10-13 01:52:44 +0800

    Reapply "Merge branch 'main' of https://github.com/Team-SE-CED/search-engine into task/10-filter-search-match"
    This reverts commit 73b63edd1965358554bcf9be2e066ca733144221.

--- components/search/area/MainTitle.vue
+++ components/search/area/MainTitle.vue
@@ -1,11 +1,16 @@
 <template>
-  <h1>Silliman University College of Engineering and Design</h1>
-  <h2>RESEARCH CATALOG</h2>
+  <div class="h-overflow">
+    <h1>Silliman University College of Engineering and Design</h1>
+    <h2>RESEARCH CATALOG</h2>
+  </div>
 </template>
 
 <script lang="ts"></script>
 
 <style scoped>
+.h-overflow {
+  z-index: -5;
+}
 h2 {
   font-family: "Times New Roman", Times, serif;
 }
